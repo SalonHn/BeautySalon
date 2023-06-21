3f66e724f357927baf629a93fa3d4a9c2d624fd9
Administrator <salonbeauty365hn@gmail.com>
2023-06-21 23:01:08 +0000

    base de datos

---
+++ BeautySalon/Models/DataBase/BeautysalonContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeautySalon.Models.Database;
+
+public partial class BeautysalonContext : DbContext
+{
+    public BeautysalonContext()
+    {
+    }
+
+    public BeautysalonContext(DbContextOptions<BeautysalonContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=beautysalon.c6boib1qjmh9.us-east-1.rds.amazonaws.com; user=sa; password=salon123; database=beautysalon; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.IdUser).HasName("PK__users__3717C98265888B73");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.IdUser)
+                .ValueGeneratedNever()
+                .HasColumnName("idUser");
+            entity.Property(e => e.DateCreate)
+                .HasColumnType("date")
+                .HasColumnName("dateCreate");
+            entity.Property(e => e.UserName)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("userName");
+            entity.Property(e => e.UserPassword)
+                .HasMaxLength(32)
+                .IsUnicode(false)
+                .HasColumnName("userPassword");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ BeautySalon/Models/DataBase/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeautySalon.Models.Database;
+
+public partial class User
+{
+    public int IdUser { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string UserPassword { get; set; } = null!;
+
+    public DateTime DateCreate { get; set; }
+}
