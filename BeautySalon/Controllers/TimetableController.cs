fa85e4dd9c69f9aa7396df23fc69190b888668e9
cedilloER <elias100cedillo@gmail.com>
2023-07-27 15:29:35 -0600

    Cambios

--- BeautySalon/Controllers/TimetableController.cs
+++ BeautySalon/Controllers/TimetableController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeautySalon.Models.DataBase;
+using BeautySalon.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BeautySalon.Controllers
 {
     public class TimetableController : Controller
     {
+        private readonly BeautysalonContext _context;
+
+        public TimetableController(BeautysalonContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var timetable = _context.Timetables.Where(timetable => timetable.IsHoliday == true).Join(
+                _context.HoursAvailables,
+                timetable => timetable.OpenHour,
+                openHour => openHour.IdHour,
+                (timetable, openHour) => new { timetable, openHour }).Join(
+                _context.HoursAvailables,
+                timetable => timetable.timetable.CloseHour,
+                closeHour => closeHour.IdHour,
+                (openTimetable, closeHour) => new {openTimetable, closeHour}).ToList();
+
+            List<ViewModelHorario> horarios = timetable.ConvertAll(x =>
+            new ViewModelHorario
+            {
+                Id = x.openTimetable.timetable.IdTimetable,
+                day = x.openTimetable.timetable.Day,
+                open = x.openTimetable.openHour.Hour,
+                close = x.closeHour.Hour
+            });
+
+            ViewBag.Timetable = horarios;
+
             return View();
         }
     }
---
+++ BeautySalon/Models/ViewModels/ViewModelHorario.cs
@@ -0,0 +1,10 @@
+﻿namespace BeautySalon.Models.ViewModels
+{
+    public class ViewModelHorario
+    {
+        public int Id { get; set; }
+        public string? day { get; set; }
+        public string? open { get; set; }
+        public string? close { get; set; }
+    }
+}
