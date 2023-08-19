1c5d40f0271e565dd0dbbadda91d9d084e90b911
cedilloER <elias100cedillo@gmail.com>
2023-08-19 17:50:42 -0600

    pago de reserva desde el cliente

--- BeautySalon/Controllers/DatosClienteController.cs
+++ BeautySalon/Controllers/DatosClienteController.cs
@@ -172,5 +172,109 @@ namespace BeautySalon.Controllers
             }
         }
 
+        public IActionResult PagarReserva(int idReserva)
+        {
+            //Datos del metodo de pago
+            int idCliente = Int32.Parse(User.FindFirst("idUser").Value);
+            InformacionDePago? infoPago = _context.InformacionDePagos.Where(ip => ip.IdUser == idCliente).FirstOrDefault();
+            ViewBag.InfoPagos = infoPago;
+
+            //Datos de la reserva
+            Reserva? reserva = _context.Reservas.Find(idReserva);
+
+            if (reserva != null)
+            {
+                ViewBag.Reserva = reserva;
+
+                Product? servcio = _context.Products.Find(reserva.IdServicio);
+                ViewBag.Servicio = servcio;
+
+                if (servcio != null)
+                {
+                    RoleEmployee? skill = _context.RoleEmployees.Find(servcio.IdSkill);
+                    ViewBag.Skill = skill;
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult ConfirmarPago(int idReserva, int idInfo)
+        {
+            try
+            {
+                int idUser = Int32.Parse(User.FindFirst("idUser").Value);
+
+                Reserva? reserva = _context.Reservas.Find(idReserva);
+                reserva.IdEstado = 3;
+                Product? servicio = _context.Products.Find(reserva.IdServicio);
+
+                InformacionDePago? info = _context.InformacionDePagos.Find(idInfo);
+                Pago pago = new Pago();
+                if (info != null)
+                {
+                    string ultimos = "xxxx xxxx xxxx ";
+                    int len = info.NumeroTarjeta.Length;
+                    for (int i = 4; i >= 1; i--)
+                    {
+                        ultimos = ultimos + info.NumeroTarjeta[len - i];
+                    }
+
+                    //Agregando el pago
+                    pago = new Pago
+                    {
+                        IdTipoPago = 2,
+                        Monto = servicio.Price * 1.15m,
+                        NumeroDeTarjeta = ultimos,
+                        Vence = info.Vence,
+                        Ccv = info.Ccv,
+                        Recibido = 0,
+                        Cambio = 0
+                    };
+                    var _pago = _context.Pagos.Add(pago);
+                    _context.SaveChanges();
+
+                    //Creando la factura 
+                    string nReferencia = (_context.Invoices.Count() + 1000000).ToString();
+                    Customer? cliente = _context.Customers.Where(c => c.IdUser == idUser).FirstOrDefault();
+                    Invoice factura = new Invoice
+                    {
+                        ReferencesNumber = nReferencia,
+                        IdCustomer = cliente.IdCustomer,
+                        NameCustomer = cliente.FullName,
+                        IdPago = _pago.Entity.IdPago,
+                        DateInvoice = DateTime.Now,
+                        Subtotal = servicio.Price,
+                        Tax = servicio.Price * 0.15m,
+                        Total = servicio.Price * 1.15m,
+                        Discount = 0
+                    };
+                    var _factura = _context.Invoices.Add(factura);
+                    _context.SaveChanges();
+
+                    //Agregando el datalle
+                    InvoiceDetail detalle = new InvoiceDetail 
+                    {
+                        IdInvoice = _factura.Entity.IdInvoice,
+                        IdProduct = servicio.IdProduct,
+                        Quantity = 1,
+                        Price = servicio.Price,
+                        Tax = (double)(servicio.Price * 0.15m),
+                        IdTypeTax = 1
+                    };
+                    _context.InvoiceDetails.Add(detalle);
+                    _context.SaveChanges();
+
+                    _metodos.addBitacora(idUser, 1, "Pago de reserva", "Se pago la reserva con id " + reserva.IdReserva);
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                int idUser = Int32.Parse(User.FindFirst("idUser").Value);
+                _metodos.addBitacora(idUser, 3, "Error al pagar reserva", "Error: " + ex.Message);
+                return RedirectToAction("Index", "Clientes");
+            }
+        }
     }
 }
